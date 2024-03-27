1a2d3993e88571462b302ed48595e5495a375a45
IlushkaDV <magnum301076@gmail.com>
2024-03-28 05:32:40 +1000

    Перенос на гит

---
+++ css/js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var beeLink = document.getElementById('beeLink');
+    var beeImg = document.getElementById('beeImg');
+
+    beeLink.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        beeImg.style.display = "block";
+        beeImg.style.left = "100%";
+
+        beeImg.style.animation = 'none';
+
+        setTimeout(function() {
+            beeImg.style.animation = 'fly 10s linear forwards';
+        }, 100);
+    });
+});
